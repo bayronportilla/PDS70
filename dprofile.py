dc52970d5fd7a262117ec29efd0d9e0d38adc558
B.A. Portilla Revelo <bportilla@auwers.intra.astro.rug.nl>
2019-09-17 18:42:55 +0200

    kapteyn

--- dprofile.py
+++ dprofile.py
@@ -7,13 +7,13 @@ mu=0.0
 sigma0=0.0059 # Keppler
 Rc=40.0 # Keppler 
 hp=0.1 # assian team
-mb=4.0*(1.898e27/1.989e30) 
-mc=4.0*(1.898e27/1.989e30) 
+m_b=4.0*(1.898e27/1.989e30) 
+m_c=4.0*(1.898e27/1.989e30) 
 ms=0.76 
 Nplanets=2
 alpha=1e-3 # assian team
-a_b=20
-a_c=30.8
+a_b=20.5
+a_c=35.8
 
 density=[]
 randomList = ['a', 0, 2]
@@ -22,14 +22,17 @@ def udens(r):
     value=sigma0*Rc/r*np.exp(-r/Rc)
     return value
 
+# perturbed density (gap_height)
 def pdens(r,mp):
     K=(mp/ms)**2*hp**-5*alpha**-1
     value=udens(r)/(1+0.04*K)
     return value
 
+"""
 def gap_width(ap,mp):
     value=0.41*ap*(mp/ms)**0.5*hp**-0.75*alpha**-0.25
     return value
+"""
 
 """
 # Dust Density Profile
@@ -41,12 +44,80 @@ def ddp(r,a_p):
         value=pdens(r)
     else:
         value=udens(r)
-    return value
+   return value
 """
 density_profile=[]
+r_array=[]
 
-width_b=gap_width(a_b,m)
-width_c=gap_width(a_c)
+class Source:
+    def __init__(self,mass,pos):
+        self.mass=mass
+        self.pos=pos
+    def gap_width(self):
+        value=0.41*self.pos*(self.mass/ms)**0.5*hp**-0.75*alpha**-0.25
+        return value 
+    def gap_height(self):
+        K=(self.mass/ms)**2*hp**-5*alpha**-1
+        value=udens(self.pos)/(1+0.04*K)
+        return value
+    def gap_limits(self):
+        width=0.41*self.pos*(self.mass/ms)**0.5*hp**-0.75*alpha**-0.25
+        ll=self.pos-0.5*width
+        rl=self.pos+0.5*width
+        return [ll,rl]
+
+source_b=Source(m_b,a_b)
+source_c=Source(m_c,a_c)
+
+# If(source_c.gap_limits()[0]<source_b.gap_limits()[1]):
+if(source_c.gap_limits()[0]<source_b.gap_limits()[1]): # Simple overlapping condition
+    for r in xval:
+        # Zone 1
+        if r<source_b.gap_limits()[0] or r>source_c.gap_limits()[1]:
+            density_profile.append(udens(r))
+            r_array.append(r)
+        # Zone II
+        elif source_b.gap_limits()[0]<r<source_c.gap_limits()[0]:
+            density_profile.append(pdens(r,source_b.mass))
+            r_array.append(r)
+        # Zone III
+        elif source_c.gap_limits()[0]<r<source_b.gap_limits()[1]:
+            value=pdens(r,source_b.mass)+pdens(r,source_c.mass)-udens(r)
+            if value<0.0:
+                density_profile.append(0.0)
+            else:
+                density_profile.append(value)
+            r_array.append(r)
+        # Zone IV
+        elif source_b.gap_limits()[1]<r<source_c.gap_limits()[1]:
+            density_profile.append(pdens(r,source_c.mass))
+            r_array.append(r)
+else: # No overlapping
+    for r in xval:
+        if source_b.gap_limits()[0]<r<source_b.gap_limits()[1]:
+            density_profile.append(pdens(r,source_b.mass))
+            r_array.append(r)
+        elif source_c.gap_limits()[0]<r<source_c.gap_limits()[1]:
+            density_profile.append(pdens(r,source_c.mass))
+            r_array.append(r)
+        else:
+            density_profile.append(udens(r))
+            r_array.append(r)
+            
+        
+density_profile=np.array(density_profile)
+r_array=np.array(r_array)
+
+plt.plot(r_array,density_profile)
+plt.xscale('log')
+plt.yscale('log')
+plt.ylim(-10.0,10)
+plt.show()
+
+sys.exit()
+
+width_b=gap_width(a_b,mb)
+width_c=gap_width(a_c,mc)
 wl_b=a_b-0.5*width_b
 wr_b=a_b+0.5*width_b
 wl_c=a_c-0.5*width_c
@@ -62,11 +133,6 @@ for r in xval:
     
     
     
-density_profile=np.array(density_profile)
-plt.plot(xval,density_profile)
-plt.xscale('log')
-plt.yscale('log')
-plt.show()
 
 
 
