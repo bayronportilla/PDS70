85f2ca059a67ee0f03bf7412c8adf0d48b53c68b
bportilla <b.a.portilla.revelo@rug.nl>
2019-12-04 13:11:27 +0100

    radial profile alma image

--- observations/brightness_profiles_observations.py
+++ observations/brightness_profiles_observations.py
@@ -7,26 +7,14 @@ import sys
 import matplotlib.ticker as ticker
 from photutils import EllipticalAnnulus
 from photutils import aperture_photometry
-#plt.style.use('fancy')
+plt.style.use('fancy')
 
-
-############################################################
-# Fancy style
-plt.rc('font', **{'family': 'family', 'family': ['serif']})
-plt.rc('text', usetex=True) #uses Latex instead of Tex to compile axes labels
-
-def radial_profile_observation(file,mode):
+def radial_profile_observation(file,mode,theta,PA):
 
     # Loading FITS image
     hdulist=fits.open(file)
-    #data=(hdulist[0].data)[0][0]
-    data=(hdulist[0].data)
+    data=(hdulist[0].data)[0][0]
 
-    #plt.imshow(data,origin="lower")
-    #plt.show()
-    #sys.exit()
-
-    """
     # Removing NaN
     ave_flux=1e-5
     for i in range(0,data.shape[0]):
@@ -35,17 +23,22 @@ def radial_profile_observation(file,mode):
                 data[i][j]=ave_flux
             else:
                 continue
-    """
     
     ############################################################
     # Input params
-    #pixel_scale=0.02 # arcsec per pixel
-    pixel_scale=0.01226 # arcsec per pixel
-    d=113.43 # Distance 
-    PA=158.6 # Position angle
-    #e=0.775 # eccentricity of the annulus
-    e=0.767 # eccentricity of the annulus
+    pxsize=20.0/1000 # arcsecs per pixel
+    d=113.43 # Distance in pc 
+    angle_annulus=((PA-90.0)*units.deg).to(units.rad).value # Position angle
+    e=np.sin((theta*units.deg).to(units.rad).value) # eccentricity of the annulus
     
+    ############################################################
+    # Determining limit
+    linear_lim=2*(150) # AU
+    angular_lim=linear_lim/((d*units.pc).to(units.au).value) # rad
+    angular_lim=(angular_lim*units.rad).to(units.arcsec).value # arcsec
+    pixel_lim=int(angular_lim/pxsize)
+
+    """
     ############################################################
     # Creating image
     x_array=np.linspace(0,data.shape[0],data.shape[0])
@@ -62,36 +55,44 @@ def radial_profile_observation(file,mode):
     image_1=ax.imshow(1000*data,cmap='hot',origin='lower',extent=extent_angular)
     cbar=fig.colorbar(image_1,pad=0.0,aspect=25)
     #cbar.ax.set_ylabel('Intensity (mJy/beam)')
-    #ax.set_xlim(1.5,-1.5)
-    #ax.set_ylim(-1.5,1.5)
+    #ax.set_xlim(1,-1)
+    #ax.set_ylim(-1,1)
     cbar.ax.set_ylabel('Intensity (???)')
-    ax.set_xlim(1,-1)
-    ax.set_ylim(-1,1)
+    ax.set_xlim(1.5,-1.5)
+    ax.set_ylim(-1.5,1.5)
     ax.set_xlabel(r'$\Delta \mathrm{RA}$ (arcsec)')
     ax.set_ylabel(r'$\Delta \mathrm{Dec}$ (arcsec)')
     fig.savefig('%s_image.png'%file)
-
+    """
     
     ############################################################
     # Creating brightness profile
-    theta_phot=((PA+90)*units.deg).to(units.rad) # angle for photometry measured form x-axis
-    x_center=0.5*data.shape[0] # Image center in data coordinates
-    y_center=0.5*data.shape[1] # Image center in data coordinates
-    dr=1 # Width of the annulus
+    xc=0.5*data.shape[0] # Image center in data coordinates
+    yc=0.5*data.shape[1] # Image center in data coordinates
+    dr=0.1 # Width of the annulus
     ain_array=[] # array of inner semi-major axis
     r_array=[] # array of (linear) radial distance
-    for i in np.arange(x_center+dr,data.shape[0],dr): 
-        ain_array.append(i-x_center)
+    
+    for i in np.arange(xc+dr,xc+0.5*pixel_lim,dr): 
+        ain_array.append(i-xc)
     aout_array=[i+dr for i in ain_array]
     bout_array=[i*(1-e**2)**0.5 for i in aout_array]
    
-    # Creating apertures
-    apertures=[EllipticalAnnulus((x_center,y_center),a_in=ain,a_out=aout,b_out=bout,theta=theta_phot.value)
-               for (ain,aout,bout) in zip(ain_array,aout_array,bout_array)]
+    # Radial distance of each annulus
+    r_arcsec=[(j+0.5*(i-j))*pxsize for (i,j) in zip(aout_array,ain_array)] # arcsec
+    r_rad=[(i*units.arcsec).to(units.rad).value for i in r_arcsec] # rad
+    r_au=[((i*d)*units.pc).to(units.au).value for i in r_rad] # AU
 
-    r_array=[((i-j)*0.5+j)*pixel_scale for i,j in zip(aout_array,ain_array)]
-    r_array=[(i*units.arcsec).to(units.rad).value for i in r_array]
-    r_array=[i*((d*units.pc).to(units.au).value) for i in r_array]
+    # Creating numpy arrays
+    ain_array=np.array(ain_array)
+    aout_array=np.array(aout_array)
+    bout_array=np.array(bout_array)
+    r_au=np.array(r_au)
+    r_arcsec=np.array(r_arcsec)
+
+    # Creating elliptical annulus apertures
+    apertures=[EllipticalAnnulus((xc,yc),a_in=ain,a_out=aout,b_out=bout,theta=angle_annulus)
+               for (ain,aout,bout) in zip(ain_array,aout_array,bout_array)]
 
     phot_table=aperture_photometry(data,apertures)
     col_values=[]
@@ -104,16 +105,23 @@ def radial_profile_observation(file,mode):
     # Creating brightness profile
     fig=plt.figure()
     ax=plt.axes()
-    ax.plot(r_array,brightness)
+    ax.plot(r_au,brightness/max(brightness),'-')
     ax.set_xlabel(r"$r$ (AU)")
     ax.set_ylabel("Intensity (???)")
-    #ax.set_xlim(0,150)
-    ax.set_xlim(0,110)
-    fig.savefig('%s_radial_profile.png'%file)
+    
+    ax.set_ylim(0,3)
+
+    
+    #ax.set_xlim(0,110)
+    fig.savefig('radial_profile.png')
           
+    file=open('radial_profile.dat',"w")
+    for i in range(0,len(r_au)):
+        file.write('%.15e %.15e \n'%(r_au[i],brightness[i]))
+
     return "Files generated!"
 
-#radial_profile_observation("PDS70_cont-final.fits",'Q')
-radial_profile_observation("PDS_70_2016-03-26_QPHI_CROPPED.fits",'Q')
+radial_profile_observation("PDS70_cont-final.fits",'Q',49.7,158.6)
+#radial_profile_observation("PDS_70_2016-03-26_QPHI_CROPPED.fits",'Q')
     
 
